6dddb2f158b2f98ffc161fda7351c8d43dc54a18
andreea10 <56259361+andreea10@users.noreply.github.com>
2019-12-02 13:19:14 +0200

    Add files via upload

--- html2/SVG/svg.js
+++ html2/SVG/svg.js
@@ -3,16 +3,16 @@ document.getElementById("id_logic_version").innerHTML = "Logic Version = 2019.10
 window.addEventListener("deviceorientation", on_orientation_uab);
 window.addEventListener("devicemotion", on_motion_uab);
 
-function desenare(beta, gamma)
+function desenare(unghi_x, unghi_y)
 {
 	var circle = document.getElementById("id_circle");
 	var svg=document.getElementById("id_svg");
 	
 	
 	
-	
-	var x = svg.width / 2 + beta / 90 * svg.width / 2 ;
-	var y = svg.height / 2 + gamma / 90 * svg.height / 2 ;
+	var r=circle.getAttribute("r")
+	var x =unghi_x/90* (svg.width / 2 -r )+  svg.width / 2 ;
+	var y = unghi_y/90* (svg.width / 2 -r )+  svg.width / 2 ;
 	
 	circle.setAttribute("cx", x);
 	circle.setAttribute("cy", y);
@@ -30,7 +30,7 @@ function on_motion_uab(e)
 	var acc = e.accelerationIncludingGravity;
 	
 	
-	var beta = -Math.atan(acc.y / acc.z) / Math.PI * 180;
-	var gamma = Math.atan(acc.x / acc.z) / Math.PI * 180;
+	var rot_x = -Math.atan(acc.y / acc.z) / Math.PI * 180;
+	var rot_y = Math.atan(acc.x / acc.z) / Math.PI * 180;
 	
 }
